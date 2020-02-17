ab724e364ac2fb29e11323cad3eb9f41d1aaf716
Ariel Briñoli <ariel0080@gmail.com>
2020-02-17 13:58:00 -0300

    MAs cambios menores

--- src/app/components/all/views/login/login.component.ts
+++ src/app/components/all/views/login/login.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { AuthService } from "src/app/services/authentication/auth.service";
-import { ToastrService } from "ngx-toastr";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/authentication/auth.service';
+import { ToastrService } from 'ngx-toastr';
 import { UserService } from 'src/app/services/firebase/user.service';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
-  public userOption: string = "none";
+  public userOption: string = 'none';
   public loading: boolean = false;
 
   constructor(
     private authService: AuthService,
-	private toastr: ToastrService,
-	private userService: UserService
+    private toastr: ToastrService,
+    private userService: UserService
   ) {}
 
   ngOnInit() {
@@ -26,105 +26,99 @@ export class LoginComponent implements OnInit {
       chkHorario: new FormControl(false, [Validators.required]),
       chkLunes: new FormControl(false, [Validators.required]),
       password: new FormControl(null, [Validators.required]),
-      captcha: new FormControl("", [Validators.required])
+      captcha: new FormControl('', [Validators.required])
     });
   }
 
   public onSubmit() {
-	this.loading = true;
-	const usr = this.loginForm.get("email").value;
-    const pass = this.loginForm.get("password").value;
+    this.loading = true;
+    const usr = this.loginForm.get('email').value;
+    const pass = this.loginForm.get('password').value;
     let restriccion = this.restriccionHoraria(
-      this.loginForm.get("chkHorario").value
+      this.loginForm.get('chkHorario').value
     );
 
-    // this.authService.LoginWithEmail(this.loginForm.get("email").value,this.loginForm.get("password").value).then(() => {
-    //     this.toastr.success("¡Bienvenido!");
-    //   }).catch(() => {
-    //     this.toastr.error("Usuario y/o contraseña incorrecto.");
-    //   });
+
 
     this.userService
       .GetUserByEmail(usr)
       .then(usuario => {
-        console.log("esta en la base");
-        if (!usuario.deleted && usuario.state == "habilitado") {
-          if (usuario.role == "cliente" && !restriccion) {
+        console.log('esta en la base');
+        if (!usuario.deleted && usuario.state == 'habilitado') {
+          if (usuario.role == 'cliente' && !restriccion) {
             this.toastr.info(
-              "El horario de atencion es de Miercoles a Domingo de 18:00 a 2:00"
+              'El horario de atencion es de Miercoles a Domingo de 18:00 a 2:00'
             );
             setTimeout(() => {
               this.loading = false;
-              this.loginForm.get("email").setValue("");
-              this.loginForm.get("password").setValue("");
-              this.loginForm.get("email").enable();
-              this.loginForm.get("password").enable();
+              this.loginForm.get('email').setValue('');
+              this.loginForm.get('password').setValue('');
+              this.loginForm.get('email').enable();
+              this.loginForm.get('password').enable();
             }, 2000);
           } else {
             this.authService
               .LoginWithEmail(usr, pass)
               .then(() => {
-                this.toastr.success("¡Bienvenido!");
+                this.toastr.success('¡Bienvenido!');
               })
               .catch(() => {
-                console.log("no estaba auth asi que lo creo");
+                console.log('no estaba auth asi que lo creo');
                 this.authService.RegisterWithEmailAdmin(usr);
               });
           }
         } else {
-          this.toastr.error("Usuario deshabilitado o inexistente");
+          this.toastr.error('Usuario deshabilitado o inexistente');
         }
       })
-      .catch(() => this.toastr.error("Usuario y/o contraseña incorrecto."))
+      .catch(() => this.toastr.error('Usuario y/o contraseña incorrecto.'))
       .finally(() => {
-        //this.loading = false;
-        //console.log("lo corto");
-        this.userOption = "none";
-        this.loginForm.get("email").disable();
-        this.loginForm.get("password").disable();
-        //this.loginForm.get('email').setValue('');
-        // this.loginForm.get('password').setValue('');
+
+        this.userOption = 'none';
+        this.loginForm.get('email').disable();
+        this.loginForm.get('password').disable();
+
       });
   }
 
   private ue(u: string) {
     this.userOption = u;
-    console.log("esta en la base", u);
+    console.log('esta en la base', u);
     this.LoadUser();
   }
 
   private LoadUser() {
     let isNull = false;
     switch (this.userOption) {
-      case "cliente1":
-        this.loginForm.get("email").setValue("cliente1@gmail.com");
+      case 'cliente1':
+        this.loginForm.get('email').setValue('cliente1@gmail.com');
         break;
-      case "cliente2":
-        this.loginForm.get("email").setValue("cliente2@gmail.com");
+      case 'cliente2':
+        this.loginForm.get('email').setValue('cliente2@gmail.com');
         break;
-      case "mozo":
-        this.loginForm.get("email").setValue("mozo@gmail.com");
+      case 'mozo':
+        this.loginForm.get('email').setValue('mozo@gmail.com');
         break;
-      case "bartender":
-        this.loginForm.get("email").setValue("bartender@gmail.com");
+      case 'bartender':
+        this.loginForm.get('email').setValue('bartender@gmail.com');
         break;
-      case "cervecero":
-        this.loginForm.get("email").setValue("cervecero@gmail.com");
+      case 'cervecero':
+        this.loginForm.get('email').setValue('cervecero@gmail.com');
         break;
-      case "cocinero":
-        this.loginForm.get("email").setValue("cocinero@gmail.com");
+      case 'cocinero':
+        this.loginForm.get('email').setValue('cocinero@gmail.com');
         break;
-      case "socio":
-        this.loginForm.get("email").setValue("socio@gmail.com");
+      case 'socio':
+        this.loginForm.get('email').setValue('socio@gmail.com');
         break;
 
       default:
-        this.loginForm.get("email").setValue("");
-        this.loginForm.get("password").setValue("");
+        this.loginForm.get('email').setValue('');
+        this.loginForm.get('password').setValue('');
         isNull = true;
         break;
     }
-    if (!isNull) this.loginForm.get("password").setValue("password");
+    if (!isNull) { this.loginForm.get('password').setValue('password'); }
   }
 
   private restriccionHoraria(chk: boolean): boolean {
@@ -134,22 +128,22 @@ export class LoginComponent implements OnInit {
     const hour = date.getHours();
     const min = date.getMinutes();
 
-    if (this.loginForm.get("chkLunes").value) {
+    if (this.loginForm.get('chkLunes').value) {
       day = 1;
     }
 
-    console.log("dia: " + day);
-    console.log("hora: " + hour);
-    console.log("minuto: " + min);
+    console.log('dia: ' + day);
+    console.log('hora: ' + hour);
+    console.log('minuto: ' + min);
 
-    if (!chk || chk == null || this.loginForm.get("chkLunes").value) {
-      if (day == 1 || day == 2) {
+    if (!chk || chk == null || this.loginForm.get('chkLunes').value) {
+      if (day === 1 || day === 2) {
         retorno = false;
       } else if (hour >= 2 && hour <= 17) {
         retorno = false;
       }
     }
-    console.log(this.loginForm.get("chkLunes").value);
+    console.log(this.loginForm.get('chkLunes').value);
     console.log(retorno);
 
     return retorno;
