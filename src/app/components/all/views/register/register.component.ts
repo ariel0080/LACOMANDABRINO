65a278cfaee0fbae76d59b3b385f33d05cd8abe0
ariel <ariel@pclab9.mecon.ar>
2020-02-18 18:46:00 -0300

    captcha propio

--- src/app/components/all/views/register/register.component.ts
+++ src/app/components/all/views/register/register.component.ts
@@ -1,53 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
-import { User, Role } from 'src/app/models/user';
-import { AuthService } from 'src/app/services/authentication/auth.service';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { User, Role } from "src/app/models/user";
+import { AuthService } from "src/app/services/authentication/auth.service";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { LoggingService } from "src/app/services/firebase/logging.service";
+import { TargetMovimiento, TipoMovimiento } from "src/app/models/logging";
 
 @Component({
-	selector: 'app-register',
-	templateUrl: './register.component.html',
-	styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent implements OnInit {
+  public registerForm: FormGroup;
+  public loading: boolean = false;
+  public random: number;
 
-	public registerForm: FormGroup;
-	public loading: boolean = false;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService,
+    private logService: LoggingService
+  ) {}
 
-	constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) {}
+  ngOnInit() {this.registerForm = new FormGroup({
+	'name': new FormControl(null, [Validators.required]),
+	'lastname': new FormControl(null, [Validators.required]),
+	'email': new FormControl(null, [Validators.required]),
+	'password': new FormControl(null, [Validators.required]),
+	'repPassword': new FormControl(null, [Validators.required]),
+	'customCaptcha': new FormControl(null),
+	'captcha': new FormControl('', [Validators.required])
+});
+this.random = this.randomNumber();
+  }
 
-	ngOnInit() 
-	{
-		this.registerForm = new FormGroup({
-		'name': new FormControl(null, [Validators.required]),
-		'lastname': new FormControl(null, [Validators.required]),
-		'email': new FormControl(null, [Validators.required]),
-		'password': new FormControl(null, [Validators.required]),
-		'repPassword': new FormControl(null, [Validators.required]),
-		'captcha': new FormControl('', [Validators.required])
-    	});
-  	}
+  onSubmit() {	
+		
+	if (this.registerForm.get('customCaptcha').value == this.random) {
 
-	onSubmit()
-	{
 		this.loading = true;
-    	let user: User = User.CreateUserWithParams(
+		let user: User = User.CreateUserWithParams(
 			this.registerForm.get('name').value,
 			this.registerForm.get('lastname').value,
 			this.registerForm.get('email').value,
 			this.registerForm.get('password').value,
 			Role.cliente
 		);
-    
-	this.authService.RegisterWithEmail(user)
-		.then(() => {
-			this.toastr.success('Te has registrado con éxito.');
-			this.router.navigate(['app/login']);
-		})
-		.catch(error => {
-			this.toastr.error(error.message, 'Ha ocurrido un error.')
-		});
-  	}
 
+		this.authService.RegisterWithEmail(user)
+			.then(() => {
+				let mensaje: string = `El usuario ${user.email} se dió de alta`;
+				this.logService.persistirMovimiento(user, TargetMovimiento.usuario, TipoMovimiento.alta, mensaje);
+				this.toastr.success('Te has registrado con éxito.');
+				this.router.navigate(['app/login']);
+			})
+			.catch(error => {
+				this.toastr.error(error.message, 'Ha ocurrido un error.')
+			});
+	} else {
+		this.toastr.info("Captcha invalido");
+	}
+}
+  private randomNumber(): number {
+    let retorno;
+
+    retorno = Math.floor(Math.random() * 6) + 1;
+
+    return retorno;
+  }
 }
